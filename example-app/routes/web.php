5ac3eaabe28fccbcd200fb37c79075af04cfdc38
adi <addierivera22@gmail.com>
2026-05-05 10:46:17 +0800

    no register (v1)

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -34,10 +34,6 @@ Route::get('/schools/{id}', [PublicSchoolController::class, 'showPublicDetail'])
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.post');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-Route::post('/register', [AuthController::class, 'register'])->name('register.post');
-
 /*
 |--------------------------------------------------------------------------
 | 3. Email Verification (Custom)
